04218907b24ce57e769ec75b5233436041ba36ee
Tamizhselvanr <rvtamizhselvan@gmail.com>
2019-03-26 11:39:52 +0530

    Added a code for union operation on sets

--- Sets/Union-in-py.py
+++ Sets/Union-in-py.py
@@ -1,5 +1,28 @@
 '''
 
+<<<<<<< HEAD
+=======
+>>> s = set("Hacker")
+>>> print s.union("Rank")
+set(['a', 'R', 'c', 'r', 'e', 'H', 'k', 'n'])
+
+>>> print s.union(set(['R', 'a', 'n', 'k']))
+set(['a', 'R', 'c', 'r', 'e', 'H', 'k', 'n'])
+
+>>> print s.union(['R', 'a', 'n', 'k'])
+set(['a', 'R', 'c', 'r', 'e', 'H', 'k', 'n'])
+
+>>> print s.union(enumerate(['R', 'a', 'n', 'k']))
+set(['a', 'c', 'r', 'e', (1, 'a'), (2, 'n'), 'H', 'k', (3, 'k'), (0, 'R')])
+
+>>> print s.union({"Rank":1})
+set(['a', 'c', 'r', 'e', 'H', 'k', 'Rank'])
+
+>>> s | set("Rank")
+set(['a', 'R', 'c', 'r', 'e', 'H', 'k', 'n'])
+
+
+>>>>>>> Added a code for union operation on sets
 The first line contains an integer, , the number of students who have subscribed to the English newspaper.
 The second line contains space separated roll numbers of those students.
 The third line contains , the number of students who have subscribed to the French newspaper.
