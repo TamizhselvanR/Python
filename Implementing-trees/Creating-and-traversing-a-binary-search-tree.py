5d267736b5363138dbb6d5307d2043d02add49f7
Tamizhselvanr <rvtamizhselvan@gmail.com>
2019-08-20 00:07:29 +0530

    Code to create and traverse a tree

---
+++ Implementing-trees/Creating-and-traversing-a-binary-search-tree.py
@@ -0,0 +1,61 @@
+from __future__ import print_function
+class node:
+    def __init__(self, info):
+        self.info = info
+        self.left = None
+        self.right = None
+class BinaryTree():
+    def __init__(self):
+        self.root = None
+
+    def create(self, val):
+        current = self.root
+        if current:
+            while (True):
+                if (val < current.info):
+                    if (current.left):
+                        current = current.left
+                    else:
+                        current.left = node(val)
+                        break
+                elif (val > current.info):
+                    if (current.right):
+                        current = current.right
+                    else:
+                        current.right = node(val)
+                        break
+        else:
+            self.root = node(val)
+
+
+def preorder(root):
+    if root:
+        print(str(root.info), end=" ")
+        fun(root.left)
+        fun(root.right)
+
+
+def postorder(root):
+    if root:
+        fun(root.left)
+        fun(root.right)
+        print(str(root.info), end=" ")
+
+
+def inorder(root):
+    if root:
+        fun(root.left)
+        print(str(root.info), end=" ")
+        fun(root.right)
+
+
+lis = [5, 3, 2, 7, 6]
+l = BinaryTree()
+for i in lis:
+    l.create(i)
+preorder(l.root)
+postorder(l.root)
+inorder(l.root)
+
+
+
